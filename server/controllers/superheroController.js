3721fe317e6b206ad56fd121a9c9e4915642e27e
ajummer <makeudreamempty@gmail.com>
2025-02-08 13:06:36 +0200

    feat: add endpoint for creating a superhero

--- server/controllers/superheroController.js
+++ server/controllers/superheroController.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
+const superheroService = require("../services/superheroServices.js")
 
-router.get("/", async (req, res) => {
-    res.send("It's working")
+// Endpoint for creating new superhero
+router.post("/",(req, res) => {
+    const { name, superpower, humilityScore } = req.body;
+
+  // Validate inputs
+  if (typeof name !== 'string' || name.trim() === '') {
+    return res.status(400).json({ error: 'Name must be a non-empty string.' });
+  }
+  if (typeof superpower !== 'string' || superpower.trim() === '') {
+    return res.status(400).json({ error: 'Superpower must be a non-empty string.' });
+  }
+  if (typeof humilityScore !== 'number' || humilityScore < 1 || humilityScore > 10) {
+    return res.status(400).json({ error: 'Humility score must be a number between 1 and 10.' });
+  }
+
+  // Add the superhero  and return the newly created superhero object
+const superhero = superheroService.addSuperhero(name,superpower,humilityScore)
+res.status(201).json(superhero);
 });
 
 module.exports = router;
--- server/services/superheroServices.js
+++ server/services/superheroServices.js
@@ -1,12 +1,15 @@
 const Superhero = require("../model/Superhero.js");
 
-//
+// Simulate db 
 let superheroes = [];
 
+// Get all superheroes sorted by their humility score in descending order
 exports.getAllbyDescendingHumility = () => {
   return superheroes.sort((a, b) => b.humilityScore - a.humilityScore);
 };
 
+
+// Add the new superhero to the array
 exports.addSuperhero = (name, superpower, humilityScore) => {
   const hero = new Superhero(name, superpower, humilityScore);
   superheroes.push(hero);
