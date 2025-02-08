a9d22e3775ea1760c9282eb88a6ae0f39b4563a0
ajummer <makeudreamempty@gmail.com>
2025-02-08 14:58:15 +0200

    feat: tweak ui

--- client/src/components/SuperheroList.jsx
+++ client/src/components/SuperheroList.jsx
@@ -1,13 +1,13 @@
 export default function SuperheroList({ superheroes }) {
     return (
         <div className="mt-8">
-            <h2 className="text-2xl font-bold mb-4 text-gray-800">Superhero List</h2>
+            <h2 className="text-2xl font-bold mb-4 border-b border-blue-500 text-gray-800">Superhero List</h2>
             {superheroes.length === 0 ? (
-                <p className="text-gray-500">No superheroes added yet.</p>
+                <p className="text-gray-500">No superheroes added yet</p>
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                     {superheroes.map((hero, index) => (
-                        <div key={index} className="bg-gradient-to-r from-purple-100 to-pink-100 p-4 rounded-lg shadow-md flex flex-col justify-between">
+                        <div key={index} className=" p-4 rounded-lg shadow-md shadow-blue-600 flex flex-col justify-between">
                             <div>
                                 <h3 className="font-bold text-lg text-gray-800 mb-2">{hero.name}</h3>
                                 <p className="text-gray-600 mb-1">Superpower: {hero.superpower}</p>
