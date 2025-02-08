046befa66c757d17a2dd7014efc004efdcee241f
ajummer <makeudreamempty@gmail.com>
2025-02-08 14:05:28 +0200

    feat: add modal component

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react"
+
+export default function Modal({ isOpen, onClose, children }) {
+  const modalRef = useRef()
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose()
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick)
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick)
+    }
+  }, [isOpen, onClose])
+
+  if (!isOpen) return null
+
+  return (
+    <div className="fixed inset-0 bg-black/80  backdrop-blur-sm flex justify-center items-center z-50">
+      <div ref={modalRef} className="bg-white rounded-lg p-6 w-full max-w-md shadow-xl">
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
+
