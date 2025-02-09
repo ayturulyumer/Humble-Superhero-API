8896c3bb8c1accf91a29f3b08fa6175032147e54
ajummer <makeudreamempty@gmail.com>
2025-02-09 12:59:53 +0200

    feat: add cors

--- server/config/server.js
+++ server/config/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const configureServer = (app) => {
+  app.use(cors());
   app.use(express.json()); // Parse JSON
   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
-    console.log("Superhero API configuration loaded successfully !");
+  console.log("Superhero API configuration loaded successfully !");
 };
 
-module.exports = configureServer
+module.exports = configureServer;
