7f26f5615fc8523be5bdecce0bf021956caa6c18
ajummer <makeudreamempty@gmail.com>
2025-02-09 13:02:03 +0200

    feat: create endpoint for getting existing superheroes

--- server/controllers/superheroController.js
+++ server/controllers/superheroController.js
@@ -1,24 +1,38 @@
 const router = require("express").Router();
-const superheroService = require("../services/superheroServices.js")
+const superheroService = require("../services/superheroServices.js");
+const validateSuperheroData = require("../utils/validateSuperheroData.js")
+
+// Endpoint for getting all superheroes
+router.get("/", (req, res) => {
+  try {
+    const superheroes = superheroService.getAllbyDescendingHumility();
+    res.status(200).json(superheroes);
+  } catch (error) {
+    console.error("Error fetching superheroes:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 // Endpoint for creating new superhero
-router.post("/",(req, res) => {
-    const { name, superpower, humilityScore } = req.body;
+router.post("/", (req, res) => {
+  const { name, superpower, humilityScore } = req.body;
 
   // Validate inputs
-  if (typeof name !== 'string' || name.trim() === '') {
-    return res.status(400).json({ error: 'Name must be a non-empty string.' });
-  }
-  if (typeof superpower !== 'string' || superpower.trim() === '') {
-    return res.status(400).json({ error: 'Superpower must be a non-empty string.' });
-  }
-  if (typeof humilityScore !== 'number' || humilityScore < 1 || humilityScore > 10) {
-    return res.status(400).json({ error: 'Humility score must be a number between 1 and 10.' });
+  const validationErrors = validateSuperheroData(name, superpower, humilityScore);
+
+  // If any errors , return the errors
+  if (validationErrors.length > 0) {
+    return res.status(400).json({ errors: validationErrors });
   }
 
-  // Add the superhero  and return the newly created superhero object
-const superhero = superheroService.addSuperhero(name,superpower,humilityScore)
-res.status(201).json(superhero);
+  // Try to add the newly created hero and return it
+  try {
+    const superhero = superheroService.addSuperhero(name, superpower, humilityScore);
+    res.status(201).json(superhero);
+  } catch (error) {
+    console.error("Error creating superhero:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
