f059eef10676130113b1cdce7d73b42cf1e6e586
ajummer <makeudreamempty@gmail.com>
2025-02-09 14:18:15 +0200

    refactor: standardize responses

--- server/controllers/superheroController.js
+++ server/controllers/superheroController.js
@@ -1,34 +1,42 @@
 const router = require("express").Router();
 const superheroService = require("../services/superheroServices.js");
-const validateSuperheroData = require("../utils/validateSuperheroData.js")
+const validateSuperheroData = require("../utils/validateSuperheroData.js");
 
 // Endpoint for getting all superheroes
 router.get("/", (req, res) => {
   try {
     const superheroes = superheroService.getAllbyDescendingHumility();
-    res.status(200).json(superheroes);
+    res.status(200).json({ data: superheroes });
   } catch (error) {
     console.error("Error fetching superheroes:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// Endpoint for creating new superhero
+// Endpoint for creating a new superhero
 router.post("/", (req, res) => {
   const { name, superpower, humilityScore } = req.body;
 
   // Validate inputs
-  const validationErrors = validateSuperheroData(name, superpower, humilityScore);
+  const validationErrors = validateSuperheroData(
+    name,
+    superpower,
+    humilityScore
+  );
 
-  // If any errors , return the errors
+  // If there are validation errors, return them
   if (validationErrors.length > 0) {
     return res.status(400).json({ errors: validationErrors });
   }
 
-  // Try to add the newly created hero and return it
   try {
-    const superhero = superheroService.addSuperhero(name, superpower, humilityScore);
-    res.status(201).json(superhero);
+    // Add the new superhero and return the superhero object
+    const superhero = superheroService.addSuperhero(
+      name,
+      superpower,
+      humilityScore
+    );
+    res.status(201).json({ data: superhero });
   } catch (error) {
     console.error("Error creating superhero:", error);
     res.status(500).json({ error: "Internal Server Error" });
